0ef02f1056b5d6e48ba0108e7fde06c8cc7cde6b
bubbkis <bubbkis@gmail.com>
2018-06-01 11:38:59 +0900

    Modify 4_setup_virtualenv_neovim.sh

--- 4_setup_virtualenv_neovim.sh
+++ 4_setup_virtualenv_neovim.sh
@@ -5,6 +5,8 @@
 #
 # virtualenv's python2 for dstat
 echo " ----------- virtualenv for neovim ------------"
+python_latest=$(pyenv install -l | grep -v '[a-z]' | tail -1 | sed 's/ //g')
+python_v2=$(pyenv install -l | grep -v '[a-z]' | grep '2.7.[0-9]*' | tail -1 | sed 's/ //g')
 pyenv virtualenv $python_v2 neovim2
 pyenv virtualenv $python_latest neovim3
 pyenv activate neovim2
